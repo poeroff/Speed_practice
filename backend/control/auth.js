3483c167af687beaf0cb82afd4fd1fbb2ddd25e7
dnjs5432 <dnjs5432@naver.com>
2023-11-22 01:34:29 +0900

    ..

--- backend/control/auth.js
+++ backend/control/auth.js
@@ -9,6 +9,6 @@ exports.postsign = async (req, res) => {
         return res.status(400).json({ errorMessage: "중복된 아이디입니다." });
     }
     if (CheckPassword !== Password) {
-        return res.status(400).json({ errorMessage: "비밀번호가 일치하지 않습니다.." });
+        return res.status(400).json({ errorMessage: "비밀번호가 일치하지 않습니다." });
     }
 };
