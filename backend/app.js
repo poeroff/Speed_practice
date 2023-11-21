d891673410ba77b152d00cbc8a3a38875fb31db4
yewon <dnjs5432@naver.com>
2023-11-21 22:02:54 +0900

    임시저장

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,18 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 
-const sequelize = require("./util/database")
+const sequelize = require("./util/database");
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
-app.post("/", (req,res,next)=>{
-    console.log("dds")
-})
-
-
-
+app.post("/", (req, res, next) => {
+    console.log("dds");
+});
 
 sequelize.sync().then((result) => {
     console.log("8080번 포트에 연결이 성공하였습니다");
     app.listen(8080);
-  });
+});
