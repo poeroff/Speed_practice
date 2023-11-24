e3a9fea2975d315559f431572b70d4fbc1155f10
Beardevelope <mck84835182@gmail.com>
2023-11-24 16:30:24 +0900

    feat : conflict resolved

--- backend/model/user.js
+++ backend/model/user.js
@@ -21,6 +21,10 @@ const User = sequelize.define("users", {
     type: Sequelize.STRING,
     allowNull: false,
   },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
 });
 
 module.exports = User;
