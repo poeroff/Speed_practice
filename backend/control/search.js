c98321af8d4e1775c1a20e1a18664eccd3e78299
dnjs5432 <dnjs5432@naver.com>
2023-11-24 17:49:08 +0900

    ..

--- backend/control/search.js
+++ backend/control/search.js
@@ -3,7 +3,7 @@ const User = require("../model/user");
 exports.searchId = async (req, res) => {
     const { search } = req.params;
     try {
-        const searchUser = await User.findOne({ where: { nickname: search } });
+        const searchUser = await User.findOne({ where: { nickname: search }, attributes: ["nickname"] });
 
         if (!searchUser) {
             return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
