a24d87dffd343d10946a7adf4df17a06f0378d47
Beardevelope <mck84835182@gmail.com>
2023-11-24 16:39:00 +0900

    feat : control/auth.js 파일 내부 회원정보 조회 및 수정 user.js에 옮겨진 것 확인되어, 삭제 처리 수정

--- backend/model/user.js
+++ backend/model/user.js
@@ -2,30 +2,29 @@ const Sequelize = require("sequelize");
 const sequelize = require("../util/database");
 
 const User = sequelize.define("users", {
-
-    userId: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-    },
-    accountId: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    nickname: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    description: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
+  userId: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  accountId: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  nickname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
 });
 
 module.exports = User;
