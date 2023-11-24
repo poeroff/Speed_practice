0d48a677cdbf1e45785b1ea4b604ac6e6e2093f1
MinJae-Mun <amvs129@naver.com>
2023-11-24 20:32:09 +0900

    feat:routes/user.js correction

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userControl = require("../control/user");
-const User = require("../model/user");
-const { check } = require("express-validator");
-const Validation = require("../middleware/validation");
 
 router.get("/userSearch/:userId", userControl.userSearch);
 router.put("/userUpdate/:userId", userControl.userUpdate);
