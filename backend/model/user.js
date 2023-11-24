5bf3eb92000806a3aee6bc4646455ec018d6f497
Beardevelope <mck84835182@gmail.com>
2023-11-24 16:26:22 +0900

    feat : 찬기 - 게시글 전체 리스트 Get method 생성 testing 완료

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,28 +1,26 @@
 const Sequelize = require("sequelize");
-
 const sequelize = require("../util/database");
 
 const User = sequelize.define("users", {
-    userId: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-    },
-    accountId: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    nickname: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    
+  userId: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  accountId: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  nickname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
 });
 
 module.exports = User;
