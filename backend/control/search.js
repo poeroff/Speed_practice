728bf4469da0433a66da6134def75a0ad0325b30
dnjs5432 <dnjs5432@naver.com>
2023-11-26 01:42:31 +0900

    ..

--- backend/control/search.js
+++ backend/control/search.js
@@ -1,6 +1,6 @@
 const User = require("../model/user");
 
-exports.searchId = async (req, res) => {
+exports.searchId = async (req, res, next) => {
     const { search } = req.params;
     try {
         const searchUser = await User.findOne({ where: { nickname: search }, attributes: ["nickname"] });
@@ -8,9 +8,26 @@ exports.searchId = async (req, res) => {
         if (!searchUser) {
             return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
         }
-        return res.status(200).json({ user: searchUser });
+
+        // return res.status(200).json({ user: searchUser });
+        next();
     } catch (error) {
-        console.error(error);
+        return res.status(500).json({ message: "서버 오류" });
+    }
+};
+
+exports.searchInfo = async (req, res) => {
+    const { search } = req.params;
+    try {
+        const userInfo = await User.findOne({
+            where: { nickname: search },
+            attributes: ["nickname", "description"],
+        });
+        if (!userInfo) {
+            return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
+        }
+        return res.status(200).json({ user: userInfo });
+    } catch (error) {
         return res.status(500).json({ message: "서버 오류" });
     }
 };
--- backend/routes/search.js
+++ backend/routes/search.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const searchControl = require("../control/search");
 
-router.get("/:search", searchControl.searchId);
+router.get("/:search", searchControl.searchId, searchControl.searchInfo);
+// router.get("/hi");
 
 module.exports = router;
